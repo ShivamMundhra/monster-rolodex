c7e0150ae52bd856dd9131f2b317f721a6c5a180
Shivam Mundhra <smundhra4478@gmail.com>
2020-04-08 02:25:27 +0530

    Updates

---
+++ precache-manifest.c60628a6234b736734b4edf0ebdccd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc6cf433863b1007d5018c6245ee9e6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "29cba2585d8ce318d483",
+    "url": "/monster-rolodex/static/css/main.fbe12603.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monster-rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cba2585d8ce318d483",
+    "url": "/monster-rolodex/static/js/main.89e30959.chunk.js"
+  },
+  {
+    "revision": "00719569f4c4503b3323",
+    "url": "/monster-rolodex/static/js/runtime-main.68db6682.js"
+  }
+]);
